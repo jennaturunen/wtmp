0bddf6f12b5503d4a7cfbda88af52d10974fe49d
jennaturunen <jenna.turunen2@gmail.com>
2020-01-28 12:56:26 +0200

    task3

--- src/index.js
+++ src/index.js
@@ -1,53 +1,21 @@
+import {createSodexoMenu, getSodexoMenu} from './modules/sodexo-data';
+import {createFazerMenu} from './modules/fazer-data';
 
 
-// Create all menus
-const menus = document.querySelectorAll('.menuList');
-
-const createAllMenus = language => {
-  for (const list of menus) {
-    list.innerHTML = '';
-    let dish;
-
-    for (let meals in LunchMenu.courses) {
-      if (language === 'FI') {
-        dish = LunchMenu.courses[meals].title_fi;
-      } else {
-        dish = LunchMenu.courses[meals].title_en;
-      }
-
-      const meal = document.createElement('p');
-      meal.textContent = dish;
-      list.appendChild(meal);
-    }
-  }
-};
-
-// By default create all menus in Finnish
-createAllMenus('FI');
-
-// Change/create one specific menu
-const changeMenu = (place, array) => {
-  place.innerHTML = '';
-
-  for (const meals of array) {
-    const meal = document.createElement('p');
-    meal.textContent = meals;
-    place.appendChild(meal);
-  }
-};
-
-// Change the language of the menu with button
+/** Change the language of every menu with the button */
 const buttons = document.querySelectorAll('.language');
 
 for (const btn of buttons) {
   btn.addEventListener('click', () => {
     if (btn.textContent === 'ENG') {
-      createAllMenus('ENG');
+      createSodexoMenu(getSodexoMenu('ENG'));
+      createFazerMenu('ENG');
       for (const btn of buttons) {
         btn.textContent = 'FI';
       }
     } else {
-      createAllMenus('FI');
+      createSodexoMenu(getSodexoMenu('FI'));
+      createFazerMenu('FI');
       for (const btn of buttons) {
         btn.textContent = 'ENG';
       }
@@ -55,7 +23,8 @@ for (const btn of buttons) {
   });
 }
 
-// Show/hide dropdown for sorting
+
+/** Show/hide dropdown for sorting */
 const dropDownBars = document.querySelectorAll('.sort');
 
 for (const dropdown of dropDownBars) {
@@ -64,7 +33,12 @@ for (const dropdown of dropDownBars) {
   });
 }
 
-// Sort menu-arrays in wanted order
+
+/**
+ * Sort menu-arrays in wanted order
+ * @param {string} sort - wanted direction to sort
+ * @param {*} array - wanted array to be sorted
+ */
 const sortArray = (sort, array) => {
   array.sort();
 
@@ -75,94 +49,14 @@ const sortArray = (sort, array) => {
   return array;
 };
 
-// Create menu in ascending order
-const ascButtons = document.querySelectorAll('.asc');
-
-for (const ascBtn of ascButtons) {
-  ascBtn.addEventListener('click', e => {
-    // Get the menuArea to display dish
-    const dropdownContentDiv = e.target.parentNode;
-    const dropDiv = dropdownContentDiv.parentNode;
-    const nav = dropDiv.parentNode;
-    const menuArea = nav.nextElementSibling.nextElementSibling;
-
-    const lang = nav.firstElementChild.innerHTML;
-
-    let lunchArray = [];
-
-    if (lang === 'ENG') {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_fi);
-      }
-    } else {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_en);
-      }
-    }
-
-    const sorted = sortArray('asc', lunchArray);
-    changeMenu(menuArea, sorted);
-  });
-}
-
-// Create menu in descending order
-const descButtons = document.querySelectorAll('.desc');
-
-for (const descBtn of descButtons) {
-  descBtn.addEventListener('click', e => {
-    // Get the menuarea to display dish
-    const dropdownContentDiv = e.target.parentNode;
-    const dropDiv = dropdownContentDiv.parentNode;
-    const nav = dropDiv.parentNode;
-    const menuArea = nav.nextElementSibling.nextElementSibling;
-
-    const lang = nav.firstElementChild.innerHTML;
-
-    let lunchArray = [];
-
-    if (lang === 'ENG') {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_fi);
-      }
-    } else {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_en);
-      }
-    }
-
-    const sorted = sortArray('desc', lunchArray);
-    changeMenu(menuArea, sorted);
-  });
-}
-
-// Select and return the random dish
+/**
+ * Select and return the random dish
+ * @param {array} array - Array from where the random dish gets picked
+ */
 const chooseRandomDish = array => {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
 
-// Choose a random dish from object and display it
-const randomButtons = document.querySelectorAll('.random');
 
-for (const randomBtn of randomButtons) {
-  randomBtn.addEventListener('click', e => {
-    let lunchArray = [];
-    const lang = e.target.previousElementSibling.textContent;
-
-    if (lang === 'FI') {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_en);
-      }
-    } else {
-      for (let lunches in LunchMenu.courses) {
-        lunchArray.push(LunchMenu.courses[lunches].title_fi);
-      }
-    }
-
-    const random = chooseRandomDish(lunchArray);
-
-    const nav = e.target.parentNode;
-    const menuArea = nav.nextElementSibling.nextElementSibling;
-    menuArea.textContent = random;
-  });
-}
+export {chooseRandomDish, sortArray};
--- src/modules/sodexo-data.js
+++ src/modules/sodexo-data.js
@@ -1,1 +1,103 @@
-import LunchMenu from './assets/json-data/sodexo.json';
+import SodexoMenu from '../assets/json-data/sodexo';
+import {chooseRandomDish, sortArray} from '../index';
+
+
+const sodexoMyyrmakiList = document.querySelector('#sodexoMyyrmaki');
+const sodexoAscBtn = document.querySelector('#sodexoAscBtn');
+const sodexoDescBtn = document.querySelector('#sodexoDescBtn');
+const sodexoRandomBtn = document.querySelector('#sodexoRandomBtn');
+const sodexoGlutenFreeBtn = document.querySelector('#sodexoGlutenFreeBtn');
+const languageBtn = document.querySelector('.language');
+
+
+/**Save menu to arrays for later use*/
+let lunchMenuArray = [];
+let glutenFreeArray = [];
+
+/**
+ * Create Sodexo-menu
+ * @param {array} menuArray - Array of every meal for the day
+ */
+const createSodexoMenu = (menuArray) => {
+  console.log('menuarray', menuArray);
+  sodexoMyyrmakiList.innerHTML = '';
+
+  for (const lunch of menuArray) {
+    const p = document.createElement('p');
+    p.textContent = lunch;
+    sodexoMyyrmakiList.appendChild(p);
+  }
+};
+
+
+/**
+ * Get SodexoMenu-array in selected language and save to array(s)
+ * @param {string} language - Wanted language of the menu
+ */
+const getSodexoMenu = (language) => {
+  lunchMenuArray = [];
+  glutenFreeArray = [];
+
+  for (const lunches in SodexoMenu.courses) {
+    if (language === 'FI') {
+      lunchMenuArray.push(SodexoMenu.courses[lunches].title_fi);
+    } else {
+      lunchMenuArray.push(SodexoMenu.courses[lunches].title_en);
+    }
+
+    glutenFreeArray.push(SodexoMenu.courses[lunches]);
+  }
+  return lunchMenuArray;
+};
+
+
+/** Create menu in Finnish by default */
+createSodexoMenu(getSodexoMenu('FI'));
+
+
+/** Display random dish from the Sodexo-menu */
+const randomSodexoDish = () => {
+  const random = chooseRandomDish(lunchMenuArray);
+  sodexoMyyrmakiList.textContent = random;
+};
+
+sodexoRandomBtn.addEventListener('click', randomSodexoDish);
+
+
+/** Sort Sodexo-menu in descending order */
+const sortSodexoDescending = () => {
+  let sorted = sortArray('desc', lunchMenuArray);
+  createSodexoMenu(sorted);
+};
+
+sodexoDescBtn.addEventListener('click', sortSodexoDescending);
+
+
+/** Sort Sodexo-menu in ascending order */
+const sortSodexoAscending = () => {
+  let sorted = sortArray('asc', lunchMenuArray);
+  createSodexoMenu(sorted);
+};
+
+sodexoAscBtn.addEventListener('click', sortSodexoAscending);
+
+/** Display every gluten free dish of the menu */
+const displayGlutenFreeDishes = () => {
+  const glutenFreeMeals = array => array.filter(array => array.properties.includes('G'));
+  const arr = glutenFreeMeals(glutenFreeArray);
+
+  sodexoMyyrmakiList.innerHTML = '';
+
+  for (const meal of arr) {
+    if (languageBtn.textContent === 'FI') {
+      sodexoMyyrmakiList.textContent += meal.title_en + ', ';
+    } else {
+    sodexoMyyrmakiList.textContent += meal.title_fi + ', ';
+    }
+  }
+};
+
+sodexoGlutenFreeBtn.addEventListener('click', displayGlutenFreeDishes);
+
+
+export {createSodexoMenu, getSodexoMenu};
